b36f3e291f245907062eb26fb27e53bdeb02b760
unho-00 <djsgh94@naver.com>
2022-06-29 15:26:39 +0900

    feat : modules - 텍스트 특정 길이 이상 생략 함수 추가 - 가격 3자리 마다 콤마 함수 추가 - 공백, 아이디, 비밀번호, 휴대전화 번호 유효성 검사 함수 추가

---
+++ front_web/src/modules/convert.ts
@@ -0,0 +1,20 @@
+/*
+글자 원하는 길이 이상시 생략
+
+text - 문자열
+lenght - 원하는 길이 
+*/
+export const ellipsis = (text: string, length: number) => {
+  return text.length >= length
+    ? `${text.split("\n").join(" ").slice(0, length)}⋯`
+    : text;
+};
+
+/*
+숫자 3자리마다 콤마 찍기
+
+price - 가격 (문자열 or 숫자)
+*/
+export const priceComma = (price: number | string) => {
+  return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
