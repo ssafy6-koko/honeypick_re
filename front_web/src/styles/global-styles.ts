6bee8c1885e697a10c918a6aeb49aacf6b180f5c
Choux <choifrance@gmail.com>
2022-07-05 12:56:04 +0900

    feat : add global text styled-component

---
+++ front_web/src/styles/global-styles.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+// const Text = `
+
+// `;
+
+export const FlexRow = `
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const FlexColumn = `
+  display: flex;
+  flex-direction: column;
+`;
+
+export const BigText = styled.p`
+  font-size: ${(props) => props.theme.fonts.size.large};
+`;
+
+export const MediumText = styled.p`
+  font-size: ${(props) => props.theme.fonts.size.medium};
+`;
+
+export const SmallText = styled.small`
+  font-size: ${(props) => props.theme.fonts.size.small};
+`;
