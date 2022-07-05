6793a138a4706fca228c150dda3a1dd9fd33b469
unho-00 <djsgh94@naver.com>
2022-07-05 17:30:07 +0900

    feat : styled-components 테마 적용, reset css 적용 - styled-components 테마 적용 (ThemeProvider 설정) - reset css 적용 (기존 index.css 와 결합)

--- front_web/src/App.tsx
+++ front_web/src/App.tsx
@@ -1,18 +1,30 @@
 import { ReactNode } from 'react';
+import styled, { ThemeProvider } from 'styled-components';
 
 import Routes from './pages/Routes';
+import { FlexColumn } from './styles/global-styles';
+import { theme } from './styles/theme';
+
+const Container = styled.div`
+  ${FlexColumn}
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <Layout>
-        <Routes />
-      </Layout>
-    </div>
+    <Layout>
+      <Routes />
+    </Layout>
   );
 }
 
 function Layout({ children }: { children: ReactNode }) {
-  return <div>{children}</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Container>{children}</Container>
+    </ThemeProvider>
+  );
 }
 export default App;
