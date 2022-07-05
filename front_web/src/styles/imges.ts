64962c64318f7466db9cbfe1f1786e1f87bd9f34
unho-00 <djsgh94@naver.com>
2022-07-05 17:35:31 +0900

    feat : 정적 이미지 추가 및 상수화

---
+++ front_web/src/styles/imges.ts
@@ -0,0 +1,13 @@
+import textLogo from '../assets/images/textLogo.png';
+import goodStamp from '../assets/images/goodstamp.png';
+import honeybee from '../assets/images/honeybee.png';
+import honeyStamp from '../assets/images/honeystamp.png';
+import searchUnderLine from '../assets/images/search_underline.png';
+import search from '../assets/images/search.png';
+
+export const textLogoImg = textLogo;
+export const goodStampImg = goodStamp;
+export const honeybeeImg = honeybee;
+export const honeyStampImg = honeyStamp;
+export const searchUnderLineImg = searchUnderLine;
+export const searchImg = search;
