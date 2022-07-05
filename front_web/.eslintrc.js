8ce6fdff575cf65dbdc0abc9c3fbc8256c0c61de
unho-00 <djsgh94@naver.com>
2022-07-05 16:18:09 +0900

    setting : eslintrc.js 수정 - 작업 폴더에 따라 tsconfig.json 파일 인식 못하는 버그 수정

--- front_web/.eslintrc.js
+++ front_web/.eslintrc.js
@@ -1,3 +1,8 @@
 const createConfig = require('@titicaca/eslint-config-triple/create-config');
 
-module.exports = createConfig({ type: 'frontend', project: './tsconfig.json' });
+const wd = process.cwd().split('/').slice(-1)[0];
+
+module.exports = createConfig({
+  type: 'frontend',
+  project: `.${wd === 'honeypick_re' ? '/front_web' : ''}/tsconfig.json`,
+});
