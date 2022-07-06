8b34ada796a78f68be072ebbb883e94cd80c7788
unho-00 <djsgh94@naver.com>
2022-07-06 14:46:16 +0900

    setting : eslintrc.js 수정 (no-console off로 변경)

--- front_web/.eslintrc.js
+++ front_web/.eslintrc.js
@@ -2,7 +2,21 @@ const createConfig = require('@titicaca/eslint-config-triple/create-config');
 
 const wd = process.cwd().split('/').slice(-1)[0];
 
-module.exports = createConfig({
+const { extends: extendConfigs, overrides } = createConfig({
   type: 'frontend',
   project: `.${wd === 'honeypick_re' ? '/front_web' : ''}/tsconfig.json`,
 });
+
+module.exports = {
+  extends: [
+    ...extendConfigs,
+    // 확장할 규칙 이름...
+  ],
+  overrides: [
+    ...overrides,
+    // 특정 파일 대상 규칙...
+  ],
+  rules: {
+    'no-console': 'off',
+  },
+};
